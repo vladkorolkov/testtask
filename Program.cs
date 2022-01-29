c41d6d4b3ef281404498b589a4e6f9a4b7a90c67
Vlad <korolkovjazz@yandex.ru>
2022-01-29 11:05:26 +0300

    second

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-﻿using System.Net;
-
-WebClient client = new WebClient();
-var strPage = client.DownloadString("https://www.avtoall.ru/catalog/paz-20/avtobusy-36/paz_672m-393/");
-Console.ReadLine();
+﻿string url = "https://www.avtoall.ru/catalog/paz-20/avtobusy-36/paz_672m-393/#";
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("https://www.avtoall.ru/");
+using (HttpResponseMessage response = client.GetAsync(url).Result)
+{
+   using ( HttpContent content = response.Content)
+    {
+        string result = content.ReadAsStringAsync().Result;
+    }
+}
